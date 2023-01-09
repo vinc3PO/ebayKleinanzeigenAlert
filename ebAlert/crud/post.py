208633991883ea559258e79160f5d00a9f15f7fe
cyberpete2244 <shqfaya@gmail.com>
2023-01-09 16:14:48 +0100

    typo

--- ebAlert/crud/post.py
+++ ebAlert/crud/post.py
@@ -11,19 +11,20 @@ class CRUDPost(CRUBBase):
 
     def add_items_to_db(self, items: List[EbayItem], db: Session, simulate=False) -> List[EbayItem]:
         new_items = []
-        print("Working:", end = ' ')
+        print("Working:", end=' ')
         for item in items:
             db_result = self.get_by_key({"post_id": str(item.id)}, db)
             if not db_result:
-                print('C')
+                # new article
+                print("C", end='')
                 if not simulate:
                     self.create({"post_id": str(item.id), "price": item.price}, db=db)
                 new_items.append(item)
             else:
-                print('U')
                 old_price = str(getattr(db_result, "price"))
                 if old_price != item.price:
-                    # was there a different price before, update it and inform
+                    # there was a different price before, update it and inform
+                    print("C", end='')
                     if not simulate:
                         self.update({"post_id": str(item.id), "price": item.price}, db=db)
                     item.old_price = old_price
