270ca6a962cb9186aac07fac11cc1f7a0824aea6
cyberpete2244 <shqfaya@gmail.com>
2023-01-15 15:35:25 +0100

    updated db for location filtering, and checking distance

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ebayAlert',
-    version='1.3',
+    version='2.0',
     packages=find_packages(),
     install_requires=[
-        'click~=8.1.3'
-        'SQLAlchemy~=1.4.46'
-        'bs4~=0.0.1'
-        'beautifulsoup4~=4.11.1'
-        'geopy~=2.3.0'
-        'requests~=2.28.1'
-        'setuptools~=65.5.1'
+        'click>=8.1.3'
+        'SQLAlchemy>=1.4.46'
+        'bs4>=0.0.1'
+        'beautifulsoup4>=4.11.1'
+        'geopy>=2.3.0'
+        'requests>=2.28.1'
+        'setuptools>=65.5.1'
     ],
     entry_points={'console_scripts': 'ebAlert=ebAlert.main:cli'}
 )
